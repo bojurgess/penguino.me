ecd4296f5e41c276deb34f60b688b83cdc53b9d3
Joe Burgess <bojurgess@gmail.com>
2023-03-23 03:38:35 +0000

    move links page to correct route and redirect from index to new route

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,6 @@
+import { redirect } from '@sveltejs/kit';
+ 
 /** @type {import('./$types').PageLoad} */
-import { CurrentlyPlaying } from '$lib/stores';
-
-export async function load() {
-	await CurrentlyPlaying.refresh();
-	return;
+export function load() {
+  throw redirect(302, '/links')
 }
---
+++ src/routes/links/+page.ts
@@ -0,0 +1,7 @@
+/** @type {import('./$types').PageLoad} */
+import { CurrentlyPlaying } from '$lib/stores';
+
+export async function load() {
+	await CurrentlyPlaying.refresh();
+	return;
+}
