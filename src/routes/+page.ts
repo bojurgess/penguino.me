83f55612355ab8943c33bde79683f21e934c50e6
Joe Burgess <bojurgess@gmail.com>
2023-06-16 03:12:19 +0100

    Change Routes

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,47 +1,6 @@
-import type { PageLoad } from './$types';
-import { marked } from 'marked';
-import { SpotifyStore } from '$lib/stores';
+import { redirect } from '@sveltejs/kit';
 
-marked.setOptions({
-    mangle: false,
-    headerIds: false,
-})
-
-export const load: PageLoad = async ({ fetch }) => {
-    SpotifyStore.init(fetch);
-
-    const res = await fetch('/markdown/about.md');
-    const md = await res.text();
-
-    return {
-        avatar: 'https://cdn.penguino.me/profile/avatar.gif',
-        banner: 'https://cdn.penguino.me/profile/banner.gif',
-        markdown: marked.parse(md),
-        links: [
-            {
-                name: 'Steam',
-                href: 'https://steamcommunity.com/id/penguino__/',
-                icon: 'https://cdn.simpleicons.org/steam/ffffff',
-                tooltip: 'penguino__',
-            },
-            {
-                name: 'GitHub',
-                href: 'https://github.com/bojurgess',
-                icon: 'https://cdn.simpleicons.org/github/ffffff',
-                tooltip: 'bojurgess',
-            },
-            {
-                name: 'Discord',
-                clipboard: 'beno___',
-                icon: 'https://cdn.simpleicons.org/discord/ffffff',
-                tooltip: 'beno___',
-            },
-            {
-                name: 'Spotify',
-                href: 'https://open.spotify.com/user/8fzywdklm84r9hupsurfxdoj2',
-                icon: 'https://cdn.simpleicons.org/spotify/ffffff',
-                tooltip: '—Beno',
-            }
-        ]
-    };
+/** @type {import('./$types').PageLoad} */
+export function load() {
+	throw redirect(302, '/links');
 }
---
+++ src/routes/links/+page.ts
@@ -0,0 +1,47 @@
+import type { PageLoad } from './$types';
+import { marked } from 'marked';
+import { SpotifyStore } from '$lib/stores';
+
+marked.setOptions({
+    mangle: false,
+    headerIds: false,
+})
+
+export const load: PageLoad = async ({ fetch }) => {
+    SpotifyStore.init(fetch);
+
+    const res = await fetch('/markdown/about.md');
+    const md = await res.text();
+
+    return {
+        avatar: 'https://cdn.penguino.me/profile/avatar.gif',
+        banner: 'https://cdn.penguino.me/profile/banner.gif',
+        markdown: marked.parse(md),
+        links: [
+            {
+                name: 'Steam',
+                href: 'https://steamcommunity.com/id/penguino__/',
+                icon: 'https://cdn.simpleicons.org/steam/ffffff',
+                tooltip: 'penguino__',
+            },
+            {
+                name: 'GitHub',
+                href: 'https://github.com/bojurgess',
+                icon: 'https://cdn.simpleicons.org/github/ffffff',
+                tooltip: 'bojurgess',
+            },
+            {
+                name: 'Discord',
+                clipboard: 'beno___',
+                icon: 'https://cdn.simpleicons.org/discord/ffffff',
+                tooltip: 'beno___',
+            },
+            {
+                name: 'Spotify',
+                href: 'https://open.spotify.com/user/8fzywdklm84r9hupsurfxdoj2',
+                icon: 'https://cdn.simpleicons.org/spotify/ffffff',
+                tooltip: '—Beno',
+            }
+        ]
+    };
+}
